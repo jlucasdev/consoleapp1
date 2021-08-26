9f74f4f6828d24d29cf4973bf58e664f3440aece
João Lucas <joaolucaspdsilva1@gmail.com>
2021-08-25 22:15:44 -0300

    segundo commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            do
+            {
+                if (a == 3)
+                {
+                    break;
+                }
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a <= 10)
+        }
+    }
+}
